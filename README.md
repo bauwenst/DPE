2b0997b89fb6298ee6d0012e231ae8b10d99af6f
Thomas Bauwens <thomas.bauwens@kuleuven.be>
2025-07-31 20:45:09 +0200

    Added forwards and backwards implementation of Viterbi loss.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DPE
-Attempted working reimplementation of He et al. (2020)'s DPE tokeniser.
+Attempted working reimplementation of [He et al. (2020)](https://aclanthology.org/2020.acl-main.275/)'s DPE tokeniser.
---
+++ src/dpe/viterbi_loss.py
@@ -0,0 +1,66 @@
+import torch
+from torch import Tensor
+from torch.nn import LogSoftmax
+
+
+def viterbiSum_forward(sentence: str, hard_boundary_after: list[int], vocab: set[str], loglikelihoods: Tensor) -> Tensor:  # Predictions is an N x K grid.
+    n, max_k = loglikelihoods.shape
+    assert n == len(sentence)
+
+    log_marginal_probabilities = [0]*n  # Note that
+    # grid[end] = sum_{all segmentations of string} P(segmentation)
+    #           = sum_{prefix of string ending at j=1..n} P(string[j:]) * sum_{all segmentations of string[:j]} P(segmentation)
+    #           = sum_{j=1..n} P(string[j:]) * grid[j]
+    #
+    # If the grid contains logs, and your probabilities are only available as logs, you want to convert the intermediate formula:
+    # grid[end] = log sum_{all segmentations of string} P(segmentation)
+    #           = log sum_{prefix of string ending at j=1..n} P(string[j:]) * sum_{all segmentations of string[:j]} P(segmentation)
+    #           = log sum_{j=1..n} exp(log(P(string[j:]))) * exp(log(sum_{all segmentations of string[:j]} P(segmentation)))
+    #           = log sum_{j=1..n} exp(log(P(string[j:])) + log(sum_{all segmentations of string[:j]} P(segmentation)))
+    #           = log sum_{j=1..n} exp(logP(string[j:end]) + grid[j])
+    # which is the backwards formula in He 2020.
+    #
+    # The forwards formula would be as follows: in node i, for j > i,
+    #  grid[j] = log(  exp( grid[j] ) + exp( logP[i,j] + grid[i] )  )
+
+    for i in range(n):  # The starting index of the token.
+        for k in range(max_k):  # The length of the token.
+            if hard_boundary_after and i+k >= hard_boundary_after[0]:  # TODO: Could be off-by-one error.
+                hard_boundary_after.pop(0)
+                break  # Longer tokens are no longer allowed by the next hard boundary.
+
+            if sentence[i:i+k+1] not in vocab:
+                continue
+
+            log_marginal_probabilities[i+k+1] = torch.log( torch.exp(log_marginal_probabilities[i+k+1]) + torch.exp(log_marginal_probabilities[i] + loglikelihoods[i][k]) )
+
+    return log_marginal_probabilities[-1]
+
+
+def viterbiSum_backward(sentence: str, vocab: set[str], loglikelihoods: Tensor) -> Tensor:  # Predictions is an N x K grid.
+    n, max_k = loglikelihoods.shape
+    assert n == len(sentence)
+
+    log_marginal_probabilities = [0]*n  # Note that
+    # grid[end] = sum_{all segmentations of string} P(segmentation)
+    #           = sum_{prefix of string ending at j=1..n} P(string[j:]) * sum_{all segmentations of string[:j]} P(segmentation)
+    #           = sum_{j=1..n} P(string[j:]) * grid[j]
+    #
+    # If the grid contains logs, and your probabilities are only available as logs, you want to convert the intermediate formula:
+    # grid[end] = log sum_{all segmentations of string} P(segmentation)
+    #           = log sum_{prefix of string ending at j=1..n} P(string[j:]) * sum_{all segmentations of string[:j]} P(segmentation)
+    #           = log sum_{j=1..n} exp(log(P(string[j:]))) * exp(log(sum_{all segmentations of string[:j]} P(segmentation)))
+    #           = log sum_{j=1..n} exp(log(P(string[j:])) + log(sum_{all segmentations of string[:j]} P(segmentation)))
+    #           = log sum_{j=1..n} exp(logP(string[j:end]) + grid[j])
+    # which is the backwards formula in He 2020.
+    for i in range(n):  # The starting index of the token.
+        log_marginal_probabilities[i] = torch.log(
+            sum(
+                torch.exp(
+                    log_marginal_probabilities[i-k-1] + loglikelihoods[i-k-1][k]
+                )
+                for k in range(max_k) if sentence[i-k-1:i] not in vocab
+            )
+        )
+
+    return log_marginal_probabilities[-1]
